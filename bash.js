ac1ac5cccef612acd3b2724155fc898b2f935dfc
Joshua Johnson <jjohn8@unh.newhaven.edu>
2021-04-23 13:01:18 -0400

    separated pwd into a module

--- bash.js
+++ bash.js
@@ -4,10 +4,11 @@ process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
   if(cmd === 'pwd') {
-    console.log(`Current directory: ${process.cwd()}`)
+    pwd();
   }
 
   process.stdout.write('You typed: ' + cmd)
   process.stdout.write('\nprompt > ')
 });
 
+const pwd = require(`./pwd.js`);
---
+++ pwd.js
@@ -0,0 +1,4 @@
+module.exports = () => {
+    console.log(`Current directory: ${process.cwd()}`);
+    process.stdout.write('\nprompt > ')
+}
